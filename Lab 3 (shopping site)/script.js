27f72cf30ea62df57ac9dde17fabebc39142ed1e
AnnaIvn <98767102+AnnaIvn@users.noreply.github.com>
2022-10-06 14:35:54 +0300

    Add files via upload

--- Lab 3 (shopping site)/script.js
+++ Lab 3 (shopping site)/script.js
@@ -22,41 +22,21 @@ window.addEventListener('scroll' ,changeMenuBg);
 
 
 
-/* img appearing  start*/
-/* var scroll = window.requestAnimationFrame ||                        if it`s not available 
-    function(callback) {window.setTimeout(callback, 1000/60)};     
-
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
-
-function loop(){
-    elementsToShow.forEach(function(element) {
-        if (isElementInViewport(element)){
-            element.classList.add('is-visible');
+/* for inner-section observer start */
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
         } else {
-            element.classList.remove('is-visible');
+            entry.target.classList.remove('show');
         }
     });
-    scroll(loop);    so it would repeat function 
-}
+});
 
-loop();                                        calling function 
-
-function isElementInViewport(el) {
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
-    var rect = el.getBoundingClientRect();
-    return (
-        (rect.top <= 0 
-            && rect.bottom >= 0)
-        ||
-        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-        ||
-        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    );
-} */
-/* img appearing end*/
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+/* for inner-section observer end */
 
 
 
